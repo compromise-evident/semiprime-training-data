0523af7e18aae1dd2cba48944ecd2d981e12d5b3
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2024-10-25 19:24:37 -0600

    v5.1.0 - Option 3 - plain pq, 1 label.

--- semiprime-training-data.cpp
+++ semiprime-training-data.cpp
@@ -1,4 +1,4 @@
-// semiprime-training-data 5.0.0 - creates any semiprimes followed by           Run it: "apt install g++ geany libgmp-dev". Open the .cpp in Geany.
+// semiprime-training-data 5.1.0 - creates any semiprimes followed by           Run it: "apt install g++ geany libgmp-dev". Open the .cpp in Geany.
 //                                 more and more digits of the smaller          Append "-lgmp" to Geany's compile & build commands. Hit F9 once. F5 to run.
 //                                 factor, each line starting with label
 //                                 digit. Get also primes & composites.
@@ -63,10 +63,12 @@ int main()
 	
 	cout << "\n(1) Generate new semiprimes & factors."
 	     << "\n(2) Generate new primes & composites."
+	     << "\n(3) Generate new semiprimes labeled with"
+	     << "\n    first digit of the smaller factor."
 	
 	     << "\n\nOption: ";
 	
-	int user_option; cin >> user_option; if((user_option != 1) && (user_option != 2)) {cout << "\nInvalid.\n"; return 0;}
+	int user_option; cin >> user_option; if((user_option != 1) && (user_option != 2) && (user_option != 3)) {cout << "\nInvalid.\n"; return 0;}
 	system("mkdir training-data     -p");
 	system("mkdir training-data/raw -p");
 	
@@ -197,6 +199,64 @@ int main()
 			}
 			
 			loops++; cout << loops << " of " << primes_and_composites_for_training << "\n";
+		}
+		out_stream.close();
+	}
+	
+	
+	
+	
+	
+	//_________________________________________________Only_semiprimes________________________________________________//
+	if(user_option == 3)
+	{	srand(time(0));
+		char  p[ 50001] = {'\0'};
+		char  q[ 50001] = {'\0'};
+		char pq[200001] = {'\0'};
+		mpz_t randomness; mpz_init(randomness);
+		mpz_t prime_p   ; mpz_init(prime_p   );
+		mpz_t prime_q   ; mpz_init(prime_q   );
+		mpz_t product   ; mpz_init(product   );
+		
+		//Testing-data.
+		cout << "\nGenerating testing-data...\n";
+		out_stream.open("training-data/raw/test.txt");
+		for(int loops = 0; loops < semiprimes_for_testing;)
+		{	for(int a = 0; a < p_length; a++) {p[a] = ((rand() % 10) + 48);}     if(p[0] == '0') {p[0] = '9';} //Random p.
+			for(int a = 0; a < q_length; a++) {q[a] = ((rand() % 10) + 48);}     if(q[0] == '0') {q[0] = '9';} //Random q.
+			mpz_set_str(randomness, p, 10); mpz_nextprime(prime_p, randomness); mpz_get_str( p, 10, prime_p);  //p made prime.
+			mpz_set_str(randomness, q, 10); mpz_nextprime(prime_q, randomness); mpz_get_str( q, 10, prime_q);  //q made prime.
+			mpz_mul(product, prime_p, prime_q);                                 mpz_get_str(pq, 10, product);  //pq made.
+			int length = 0; for(int a = 0; pq[a] != '\0'; a++) {length++;} if(length != pq_length) {continue;} //Restarts if pq not pq_length.
+			
+			//Saves to file.
+			int prime_comparison = mpz_cmp(prime_p, prime_q); //Label first.
+			if(prime_comparison < 0) {out_stream << p[0] << " ";}
+			else                     {out_stream << q[0] << " ";}
+			out_stream << pq << "\n"; //The rest.
+			
+			loops++; cout << loops << " of " << semiprimes_for_testing << "\n";
+		}
+		out_stream.close();
+		
+		//Training-data (ditto but unique items.)
+		cout << "\nGenerating training-data...\n";
+		out_stream.open("training-data/raw/train.txt");
+		for(int loops = 0; loops < semiprimes_for_training;)
+		{	for(int a = 0; a < p_length; a++) {p[a] = ((rand() % 10) + 48);}     if(p[0] == '0') {p[0] = '9';} //Random p.
+			for(int a = 0; a < q_length; a++) {q[a] = ((rand() % 10) + 48);}     if(q[0] == '0') {q[0] = '9';} //Random q.
+			mpz_set_str(randomness, p, 10); mpz_nextprime(prime_p, randomness); mpz_get_str( p, 10, prime_p);  //p made prime.
+			mpz_set_str(randomness, q, 10); mpz_nextprime(prime_q, randomness); mpz_get_str( q, 10, prime_q);  //q made prime.
+			mpz_mul(product, prime_p, prime_q);                                 mpz_get_str(pq, 10, product);  //pq made.
+			int length = 0; for(int a = 0; pq[a] != '\0'; a++) {length++;} if(length != pq_length) {continue;} //Restarts if pq not pq_length.
+			
+			//Saves to file.
+			int prime_comparison = mpz_cmp(prime_p, prime_q); //Label first.
+			if(prime_comparison < 0) {out_stream << p[0] << " ";}
+			else                     {out_stream << q[0] << " ";}
+			out_stream << pq << "\n"; //The rest.
+			
+			loops++; cout << loops << " of " << semiprimes_for_training << "\n";
 		}
 		out_stream.close();
 	}
