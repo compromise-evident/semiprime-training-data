1329d9b7b8bf7a80e25730f461ed6b4b9ee06810
Nikolay Valentinovich Repnitskiy <nvrpixel@gmail.com>
2024-10-27 22:21:31 -0600

    v5.1.3 - tiny values allowed; prime length safety!

--- semiprime-training-data.cpp
+++ semiprime-training-data.cpp
@@ -1,4 +1,4 @@
-// semiprime-training-data 5.1.2 - generates any semiprimes labeled as          Run it: "apt install g++ geany libgmp-dev". Open the .cpp in Geany.
+// semiprime-training-data 5.1.3 - generates any semiprimes labeled as          Run it: "apt install g++ geany libgmp-dev". Open the .cpp in Geany.
 //                                 the first digit of their smaller             Append "-lgmp" to Geany's compile & build commands. Hit F9 once. F5 to run.
 //                                 factor. Get also primes & composites,
 //                                 and semiprimes with expanding factors.
@@ -92,12 +92,13 @@ int main()
 		cout << "\nGenerating testing-data...\n";
 		out_stream.open("training-data/raw/test.txt");
 		for(int loops = 0; loops < semiprimes_for_testing;)
-		{	for(int a = 0; a < p_length; a++) {p[a] = ((rand() % 10) + 48);}     if(p[0] == '0') {p[0] = '1';} //Random p.
-			for(int a = 0; a < q_length; a++) {q[a] = ((rand() % 10) + 48);}     if(q[0] == '0') {q[0] = '9';} //Random q.
-			mpz_set_str(randomness, p, 10); mpz_nextprime(prime_p, randomness); mpz_get_str( p, 10, prime_p);  //p made prime.
-			mpz_set_str(randomness, q, 10); mpz_nextprime(prime_q, randomness); mpz_get_str( q, 10, prime_q);  //q made prime.
-			mpz_mul(product, prime_p, prime_q);                                 mpz_get_str(pq, 10, product);  //pq made.
-			int length = 0; for(int a = 0; pq[a] != '\0'; a++) {length++;} if(length != pq_length) {continue;} //Restarts if pq not pq_length.
+		{	for(int a = 0; a < p_length; a++) {p[a] = ((rand() % 10) + 48);}     if(p[0] == '0') {p[0] = '1';}     //Random p.
+			for(int a = 0; a < q_length; a++) {q[a] = ((rand() % 10) + 48);}     if(q[0] == '0') {q[0] = '9';}     //Random q.
+			mpz_set_str(randomness, p, 10); mpz_nextprime(prime_p, randomness); mpz_get_str( p, 10, prime_p);      //p made prime.
+			mpz_set_str(randomness, q, 10); mpz_nextprime(prime_q, randomness); mpz_get_str( q, 10, prime_q);      //q made prime.
+			if((p[p_length] != '\0') || (q[q_length] != '\0')) {p[p_length] = '\0'; q[q_length] = '\0'; continue;} //Restarts if p or q too long.
+			mpz_mul(product, prime_p, prime_q);                                 mpz_get_str(pq, 10, product);      //pq made.
+			int length = 0; for(int a = 0; pq[a] != '\0'; a++) {length++;} if(length != pq_length) {continue;}     //Restarts if pq not pq_length.
 			
 			//Restarts if label not distributed 1-9.
 			char label;
@@ -118,12 +119,13 @@ int main()
 		cout << "\nGenerating training-data...\n";
 		out_stream.open("training-data/raw/train.txt");
 		for(int loops = 0; loops < semiprimes_for_training;)
-		{	for(int a = 0; a < p_length; a++) {p[a] = ((rand() % 10) + 48);}     if(p[0] == '0') {p[0] = '1';} //Random p.
-			for(int a = 0; a < q_length; a++) {q[a] = ((rand() % 10) + 48);}     if(q[0] == '0') {q[0] = '9';} //Random q.
-			mpz_set_str(randomness, p, 10); mpz_nextprime(prime_p, randomness); mpz_get_str( p, 10, prime_p);  //p made prime.
-			mpz_set_str(randomness, q, 10); mpz_nextprime(prime_q, randomness); mpz_get_str( q, 10, prime_q);  //q made prime.
-			mpz_mul(product, prime_p, prime_q);                                 mpz_get_str(pq, 10, product);  //pq made.
-			int length = 0; for(int a = 0; pq[a] != '\0'; a++) {length++;} if(length != pq_length) {continue;} //Restarts if pq not pq_length.
+		{	for(int a = 0; a < p_length; a++) {p[a] = ((rand() % 10) + 48);}     if(p[0] == '0') {p[0] = '1';}     //Random p.
+			for(int a = 0; a < q_length; a++) {q[a] = ((rand() % 10) + 48);}     if(q[0] == '0') {q[0] = '9';}     //Random q.
+			mpz_set_str(randomness, p, 10); mpz_nextprime(prime_p, randomness); mpz_get_str( p, 10, prime_p);      //p made prime.
+			mpz_set_str(randomness, q, 10); mpz_nextprime(prime_q, randomness); mpz_get_str( q, 10, prime_q);      //q made prime.
+			if((p[p_length] != '\0') || (q[q_length] != '\0')) {p[p_length] = '\0'; q[q_length] = '\0'; continue;} //Restarts if p or q too long.
+			mpz_mul(product, prime_p, prime_q);                                 mpz_get_str(pq, 10, product);      //pq made.
+			int length = 0; for(int a = 0; pq[a] != '\0'; a++) {length++;} if(length != pq_length) {continue;}     //Restarts if pq not pq_length.
 			
 			//Restarts if label not distributed 1-9.
 			char label;
@@ -161,6 +163,7 @@ int main()
 			if(which_one == 0)
 			{	for(int a = 0; a < prime_and_composite_length; a++) {p[a] = ((rand() % 10) + 48);}     if(p[0] == '0') {p[0] = '1';}     //Random p.
 				mpz_set_str(randomness, p, 10); mpz_nextprime(prime_p, randomness); mpz_get_str(p, 10, prime_p);                         //p made prime.
+				if(p[prime_and_composite_length] != '\0') {p[prime_and_composite_length] = '\0'; continue;}                              //Restarts if p too long.
 				out_stream << "1 " << p << "\n"; //Saves to file: label, prime.
 			}
 			else
@@ -184,6 +187,7 @@ int main()
 			if(which_one == 0)
 			{	for(int a = 0; a < prime_and_composite_length; a++) {p[a] = ((rand() % 10) + 48);}     if(p[0] == '0') {p[0] = '1';}     //Random p.
 				mpz_set_str(randomness, p, 10); mpz_nextprime(prime_p, randomness); mpz_get_str(p, 10, prime_p);                         //p made prime.
+				if(p[prime_and_composite_length] != '\0') {p[prime_and_composite_length] = '\0'; continue;}                              //Restarts if p too long.
 				out_stream << "1 " << p << "\n"; //Saves to file: label, prime.
 			}
 			else
@@ -219,12 +223,13 @@ int main()
 		cout << "\nGenerating testing-data...\n";
 		out_stream.open("training-data/raw/test.txt");
 		for(int loops = 0; loops < semiprimes_for_testing;)
-		{	for(int a = 0; a < p_length; a++) {p[a] = ((rand() % 10) + 48);}     if(p[0] == '0') {p[0] = '1';} //Random p.
-			for(int a = 0; a < q_length; a++) {q[a] = ((rand() % 10) + 48);}     if(q[0] == '0') {q[0] = '9';} //Random q.
-			mpz_set_str(randomness, p, 10); mpz_nextprime(prime_p, randomness); mpz_get_str( p, 10, prime_p);  //p made prime.
-			mpz_set_str(randomness, q, 10); mpz_nextprime(prime_q, randomness); mpz_get_str( q, 10, prime_q);  //q made prime.
-			mpz_mul(product, prime_p, prime_q);                                 mpz_get_str(pq, 10, product);  //pq made.
-			int length = 0; for(int a = 0; pq[a] != '\0'; a++) {length++;} if(length != pq_length) {continue;} //Restarts if pq not pq_length.
+		{	for(int a = 0; a < p_length; a++) {p[a] = ((rand() % 10) + 48);}     if(p[0] == '0') {p[0] = '1';}     //Random p.
+			for(int a = 0; a < q_length; a++) {q[a] = ((rand() % 10) + 48);}     if(q[0] == '0') {q[0] = '9';}     //Random q.
+			mpz_set_str(randomness, p, 10); mpz_nextprime(prime_p, randomness); mpz_get_str( p, 10, prime_p);      //p made prime.
+			mpz_set_str(randomness, q, 10); mpz_nextprime(prime_q, randomness); mpz_get_str( q, 10, prime_q);      //q made prime.
+			if((p[p_length] != '\0') || (q[q_length] != '\0')) {p[p_length] = '\0'; q[q_length] = '\0'; continue;} //Restarts if p or q too long.
+			mpz_mul(product, prime_p, prime_q);                                 mpz_get_str(pq, 10, product);      //pq made.
+			int length = 0; for(int a = 0; pq[a] != '\0'; a++) {length++;} if(length != pq_length) {continue;}     //Restarts if pq not pq_length.
 			
 			//Appends smaller prime to pq[].
 			int prime_comparison = mpz_cmp(prime_p, prime_q);
@@ -247,12 +252,13 @@ int main()
 		cout << "\nGenerating training-data...\n";
 		out_stream.open("training-data/raw/train.txt");
 		for(int loops = 0; loops < semiprimes_for_training;)
-		{	for(int a = 0; a < p_length; a++) {p[a] = ((rand() % 10) + 48);}     if(p[0] == '0') {p[0] = '1';} //Random p.
-			for(int a = 0; a < q_length; a++) {q[a] = ((rand() % 10) + 48);}     if(q[0] == '0') {q[0] = '9';} //Random q.
-			mpz_set_str(randomness, p, 10); mpz_nextprime(prime_p, randomness); mpz_get_str( p, 10, prime_p);  //p made prime.
-			mpz_set_str(randomness, q, 10); mpz_nextprime(prime_q, randomness); mpz_get_str( q, 10, prime_q);  //q made prime.
-			mpz_mul(product, prime_p, prime_q);                                 mpz_get_str(pq, 10, product);  //pq made.
-			int length = 0; for(int a = 0; pq[a] != '\0'; a++) {length++;} if(length != pq_length) {continue;} //Restarts if pq not pq_length.
+		{	for(int a = 0; a < p_length; a++) {p[a] = ((rand() % 10) + 48);}     if(p[0] == '0') {p[0] = '1';}     //Random p.
+			for(int a = 0; a < q_length; a++) {q[a] = ((rand() % 10) + 48);}     if(q[0] == '0') {q[0] = '9';}     //Random q.
+			mpz_set_str(randomness, p, 10); mpz_nextprime(prime_p, randomness); mpz_get_str( p, 10, prime_p);      //p made prime.
+			mpz_set_str(randomness, q, 10); mpz_nextprime(prime_q, randomness); mpz_get_str( q, 10, prime_q);      //q made prime.
+			if((p[p_length] != '\0') || (q[q_length] != '\0')) {p[p_length] = '\0'; q[q_length] = '\0'; continue;} //Restarts if p or q too long.
+			mpz_mul(product, prime_p, prime_q);                                 mpz_get_str(pq, 10, product);      //pq made.
+			int length = 0; for(int a = 0; pq[a] != '\0'; a++) {length++;} if(length != pq_length) {continue;}     //Restarts if pq not pq_length.
 			
 			//Appends smaller prime to pq[].
 			int prime_comparison = mpz_cmp(prime_p, prime_q);
